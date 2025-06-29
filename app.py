64f4595f676a1cb8de6864ded919367da873ecde
Rohit Bhandari <rohitbhandari@192.168.1.3>
2025-06-29 19:49:21 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/downloads')
+def downloads():
+    return render_template('downloads.html')
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('static/assets', filename)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
