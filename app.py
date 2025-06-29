0c5b0a65f888445fe14c596eb9091dad982546cc
Rohit Bhandari <rohitbhandari@192.168.1.3>
2025-06-29 19:50:58 +0530

    Changed host

--- app.py
+++ app.py
@@ -23,4 +23,5 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+    # app.run(host='127.0.0.1', port=5000, debug=True)
